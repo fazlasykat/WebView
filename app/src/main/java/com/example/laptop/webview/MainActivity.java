0197c924c472ce424e5392d3d12b8b57ecdddb3e
fazlasykat <fazlasykat24@gmail.com>
2018-09-13 14:42:35 +0600

    Initial commit

---
+++ app/src/main/java/com/example/laptop/webview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.laptop.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webViewId);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("http://www.facebook.com");
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+
+            webView.canGoBack();
+        }
+        else {
+
+            super.onBackPressed();
+        }
+    }
+}
